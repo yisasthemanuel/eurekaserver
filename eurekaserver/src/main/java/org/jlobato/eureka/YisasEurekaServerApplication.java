893dfd871078ba1a1c7c58737224a85a48487b78
Jesús Manuel Pérez Lobato <yisasthemanuel@gmail.com>
2023-01-06 20:08:22 +0100

    Se incluyen los endpoints para instrumentalización por prometheus y grafana, además de otros ajustes como el etiquetado de la imagen docker como lastest para la última versión

--- eurekaserver/src/main/java/org/jlobato/eureka/YisasEurekaServerApplication.java
+++ eurekaserver/src/main/java/org/jlobato/eureka/YisasEurekaServerApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class YisasEurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YisasEurekaServerApplication.class, args);
+		log.info("The Yisas Eureka Server is ready to dispatch requests!");
 	}
 
 }
